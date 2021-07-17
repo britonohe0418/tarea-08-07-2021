b92fb6ec7a81eecd770e9df4bece22c8e69c23a0
Nohelia Brito <britonohe0418@gmail.com>
2021-07-16 21:36:11 -0500

    Clase venta.

---
+++ venta.py
@@ -0,0 +1,7 @@
+class Venta:
+    def __init__(self, fac, fecha, cli, tot, venta_det):
+        self.factura = fac
+        self.fecha = fecha
+        self.cliente = cli
+        self.total = tot
+        self.venta_detalle = venta_det
