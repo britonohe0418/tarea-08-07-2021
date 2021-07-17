db745eb7cbde0f30f8cffd913cb5f1d6cb07a77c
Nohelia Brito <britonohe0418@gmail.com>
2021-07-16 21:28:01 -0500

    Clase articulo.

---
+++ articulo.py
@@ -0,0 +1,13 @@
+"""
+Articulo
+Cliente
+Venta
+VentaDet
+"""
+
+class Articulo:
+    def __init__(self, cod, des, pre, sto):
+        self.codigo = cod
+        self.descripcion = des
+        self.precio = pre
+        self.stock = sto
